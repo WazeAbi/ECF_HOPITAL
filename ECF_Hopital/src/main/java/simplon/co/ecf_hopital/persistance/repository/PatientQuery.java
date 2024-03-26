e1b09758f1a04e4be2a3f590338e12dafcd8ac67
WazeAbi <alexdalle12@gmail.com>
2024-03-26 12:03:52 +0100

    Feat(Repository/PatientQuery):Création de la classe PatientQuery

---
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/persistance/repository/PatientQuery.java
@@ -0,0 +1,5 @@
+package simplon.co.ecf_hopital.persistance.repository;
+
+public class PatientQuery {
+    public static final String REQUETE_RECUPERER_PAR_NOM = "select p from Patient p where p.nom=nom";
+}
