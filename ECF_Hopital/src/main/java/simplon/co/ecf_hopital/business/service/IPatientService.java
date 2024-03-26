5952668861a16312828d669c9fa0e294c59c517a
WazeAbi <alexdalle12@gmail.com>
2024-03-26 16:05:21 +0100

    Feat(Business/Service):Création de l'interface et implemantations des futurs methodes

--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/IPatientService.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/IPatientService.java
@@ -1,2 +1,11 @@
-package simplon.co.ecf_hopital.business.service;public interface IPatientService {
+package simplon.co.ecf_hopital.business.service;
+
+import simplon.co.ecf_hopital.business.dto.PatientDto;
+
+public interface IPatientService {
+    PatientDto recuperParNumSecuritSocial(final int numSecuriteSocial);
+    void modifierPatient(PatientDto patient);
+    void creerPatient(PatientDto patient);
+    void supprimerPatient(int id);
+
 }
--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/PatientServiceImpl.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/PatientServiceImpl.java
@@ -1,2 +1,30 @@
-package simplon.co.ecf_hopital.business.service;public class PatientServiceImpl {
+package simplon.co.ecf_hopital.business.service;
+
+import simplon.co.ecf_hopital.business.dto.PatientDto;
+
+public class PatientServiceImpl implements IPatientService {
+    @Override
+    public PatientDto recuperParNumSecuritSocial(int numSecuriteSocial) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void modifierPatient(PatientDto patient) {
+        //TODO
+
+
+    }
+
+    @Override
+    public void creerPatient(PatientDto patient) {
+        //TODO
+
+    }
+
+    @Override
+    public void supprimerPatient(int id) {
+        //TODO
+
+    }
 }
