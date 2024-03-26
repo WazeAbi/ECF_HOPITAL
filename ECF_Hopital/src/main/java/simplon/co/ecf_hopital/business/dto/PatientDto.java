409bf453d466a02c9e4e2bc577e96e34609bf4bc
WazeAbi <alexdalle12@gmail.com>
2024-03-26 16:03:30 +0100

    Feat(Business/DTO):Création du DTO Patient

--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/dto/PatientDto.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/dto/PatientDto.java
@@ -1,2 +1,12 @@
-package simplon.co.ecf_hopital.business.dto;public class PatientDto {
+package simplon.co.ecf_hopital.business.dto;
+
+import java.util.Date;
+
+public class PatientDto {
+    private int id;
+    private String nom;
+    private String prenom;
+    private int numSecuriteSocial;
+    private Date edate;
+    private Date sdate;
 }
