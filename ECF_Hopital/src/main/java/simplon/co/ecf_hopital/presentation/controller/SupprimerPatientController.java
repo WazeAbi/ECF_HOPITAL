7aff0dd9b7885b4cd01aada6f8a3ad5a8eb1b98a
WazeAbi <alexdalle12@gmail.com>
2024-03-27 15:38:21 +0100

    Feat(Presentation/Controller):Création du controller SupprimerPatientController

---
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/presentation/controller/SupprimerPatientController.java
@@ -0,0 +1,21 @@
+package simplon.co.ecf_hopital.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import simplon.co.ecf_hopital.business.service.IPatientService;
+
+@RestController
+public class SupprimerPatientController {
+    private
+    IPatientService patientService;
+   @DeleteMapping("/patients/{id}")
+   public void SupprimerPatient(@PathVariable Integer id){
+       patientService.supprimerPatient(id);
+   }
+    @Autowired
+    public void setPatientService(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+}
