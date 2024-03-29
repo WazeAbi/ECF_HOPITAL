677e78478eebfcbdff47dbb98501aaca3d8080a3
WazeAbi <alexdalle12@gmail.com>
2024-03-29 11:32:50 +0100

    Feat(Business&Convert):Création des convert + Methodes Services pour Service

--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/service/ServiceServiceImpl.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/service/ServiceServiceImpl.java
@@ -1,6 +1,9 @@
 package simplon.co.ecf_hopital.business.service.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import simplon.co.ecf_hopital.business.convert.ServiceConvert;
 import simplon.co.ecf_hopital.business.dto.ServiceDto;
+import simplon.co.ecf_hopital.persistance.entity.Service;
 import simplon.co.ecf_hopital.persistance.repository.IServiceRepository;
 
 import java.util.List;
@@ -8,15 +11,35 @@ import java.util.List;
 public class ServiceServiceImpl implements IServiceService {
     private IServiceRepository serviceRepository;
     private ServiceConvert serviceConvert;
+
     @Override
     public List<ServiceDto> ListerLesServices() {
-        //TODO
-        return null;
+        final List<Service> result = serviceRepository.findAll();
+        return ServiceConvert.getInstance().convertListEntityToListDto(result);
     }
+
 
     @Override
     public ServiceDto recupererServiceParId(int id) {
-        //TODO
-        return null;
+        return serviceConvert.convertEntityToDto(serviceRepository.getReferenceById(id));
+
+    }
+
+    @Autowired
+    public IServiceRepository getServiceRepository() {
+        return serviceRepository;
+    }
+
+    @Autowired
+    public void setServiceRepository(IServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    public ServiceConvert getServiceConvert() {
+        return serviceConvert;
+    }
+
+    public void setServiceConvert(ServiceConvert serviceConvert) {
+        this.serviceConvert = serviceConvert;
     }
 }
