0d0a3dc48376341b1e7dd68e25b5fd8dc68ff3ad
WazeAbi <alexdalle12@gmail.com>
2024-03-29 11:17:08 +0100

    Feat(Service/business): Création des Services et Dto de service

--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/dto/ServiceDto.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/dto/ServiceDto.java
@@ -1,2 +1,22 @@
-package simplon.co.ecf_hopital.business.dto;public class ServiceDto {
+package simplon.co.ecf_hopital.business.dto;
+
+public class ServiceDto {
+    private int idservice;
+    private String nomdeservice;
+
+    public int getIdservice() {
+        return idservice;
+    }
+
+    public void setIdservice(int idservice) {
+        this.idservice = idservice;
+    }
+
+    public String getNomdeservice() {
+        return nomdeservice;
+    }
+
+    public void setNomdeservice(String nomdeservice) {
+        this.nomdeservice = nomdeservice;
+    }
 }
--- ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/service/ServiceServiceImpl.java
+++ ECF_Hopital/src/main/java/simplon/co/ecf_hopital/business/service/service/ServiceServiceImpl.java
@@ -1,2 +1,22 @@
-package simplon.co.ecf_hopital.business.service.service;public class ServiceServiceImpl {
+package simplon.co.ecf_hopital.business.service.service;
+
+import simplon.co.ecf_hopital.business.dto.ServiceDto;
+import simplon.co.ecf_hopital.persistance.repository.IServiceRepository;
+
+import java.util.List;
+
+public class ServiceServiceImpl implements IServiceService {
+    private IServiceRepository serviceRepository;
+    private ServiceConvert serviceConvert;
+    @Override
+    public List<ServiceDto> ListerLesServices() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ServiceDto recupererServiceParId(int id) {
+        //TODO
+        return null;
+    }
 }
